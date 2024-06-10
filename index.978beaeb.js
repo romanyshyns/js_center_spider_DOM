43c0ce5d5d9f06a51daaf508c882f908dad69408
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-06-10 14:13:14 +0300

    make build

---
+++ index.978beaeb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientWidth-t.clientWidth)/2,l=(e.clientHeight-t.clientHeight)/2;t.style.left="".concat(c,"px"),t.style.top="".concat(l,"px");
+//# sourceMappingURL=index.978beaeb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const x = (field.clientWidth - spider.clientWidth) / 2;
+const y = (field.clientHeight - spider.clientHeight) / 2;
+spider.style.left = `${x}px`;
+spider.style.top = `${y}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
